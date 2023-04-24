7757f949ebd9e8ba6a0e25c503454c0316e20cdb
GithubHitakshi <100277458+GithubHitakshi@users.noreply.github.com>
2023-04-24 17:52:02 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+p1_name = localStorage.getItem("p1_name");
+p2_name = localStorage.getItem("p2_name");
+document.getElementById("player1_name").innerHTML = p1_name;
+document.getElementById("player2_name").innerHTML = p2_name;
+document.getElementById("q_turn").innerHTML = "Question Turn:"+ p1_name;
+document.getElementById("a_turn").innerHTML = "Answer Turn:"+ p2_name;
+p1_score = 0;
+p2_score = 0;
+document.getElementById("player1_score").innerHTML = p1_score;
+document.getElementById("player2_score").innerHTML = p2_score;
